f2b788e27cc8bc885658fb15090bf8b303aacdfd
naveenm6624 <114210890+naveenm6624@users.noreply.github.com>
2022-10-07 15:17:08 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -7,7 +7,7 @@ provider "google" {
 
 
 resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
+  name          = "auto-expiring-bucket89786"
   location      = "US"
   force_destroy = true
 
