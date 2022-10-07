f5d7c26cb1e39a1017e0a2d637b9029b11bf8933
naveenm6624 <114210890+naveenm6624@users.noreply.github.com>
2022-10-07 12:14:26 +0530

    Add files via upload

---
+++ bucket.tf
@@ -0,0 +1,22 @@
+provider "google" {
+    credentials = "keys.json"
+    project = "sturdy-ranger-355706"
+  
+}
+
+
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
